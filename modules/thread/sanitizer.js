e6a4a0d80930d4dcd1061d24821d217f81af1e3d
Minh Nguyễn <minhnguyen@Minhs-MacBook-Pro.local>
2024-07-17 22:44:09 -0700

    add things

--- modules/thread/sanitizer.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-  let { body } = req;
-
-  if (body.email) {
-    body.email = body.email.trim().toLowerCase();
-  }
-
-  next();
-};
--- modules/thread/validator.js
+++
@@ -1,8 +0,0 @@
-const validators = require("./validators");
-
-class ThreadValidator {
-  static async validateAddNewThreadData(data) {
-    return validators.addNewThread(data);
-  }
-}
-module.exports = ThreadValidator;
